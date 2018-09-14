829b849c1282325547c226f5429a46c7e2e46a27
unknown <viniciusferreirawk@gmail.com>
2018-09-13 23:21:01 -0300

    fix

--- create3-group.php
+++ create3-group.php
@@ -6,7 +6,7 @@ require_once 'utils.php';
 /**This is main script which is used for contact saving in account,variable declare before is passed here.***/
 
 $contactXML = '<?xml version="1.0" encoding="utf-8"?>
-<entry gd:etag="{lastKnownEtag}">
+<entry gd:etag="contactEtag">
   <id>http://www.google.com/m8/feeds/contacts/{userEmail}/base/{idgmail}</id>
   <gContact:groupMembershipInfo deleted="false"
     href="http://www.google.com/m8/feeds/groups/{userEmail}/base/{groupId}"/>
@@ -32,5 +32,38 @@ curl_setopt($ch, CURLOPT_FAILONERROR, true);
 $result = curl_exec($ch);
 
 var_dump($result);
-file_put_contents('user-retorno.txt', var_export($result,true));
+file_put_contents('user-group-retorno.txt', var_export($result,true));
 
+
+
+
+// PUT /m8/feeds/contacts/default/full/6976230c08eade30 HTTP/1.1
+// Host: www.google.com
+// Gdata-version: 3.0
+// Content-length: 1285
+// Content-type: application/atom+xml
+// If-match: *
+// Authorization: Bearer ya29.GlsYBlyxmqGXKU3sAw8ygVna95tw8fXhN0zskZKkwWnfams4vAFAnXzXvMAQTmWNol38-Or-ozKCPIw5o1Ot_G5NOyQj3bbJVvoS3UY_-Cq-Uv-THhfAiX9KhTld
+// <entry gd:etag="{lastKnownEtag}">
+//   <id>http://www.google.com/m8/feeds/contacts/viniciusferreirawk%40gmail.com/base/6976230c08eade30</id>
+//   <updated>2018-09-13T23:09:02.303Z</updated>
+//   <category scheme="http://schemas.google.com/g/2005#kind"
+//     term="http://schemas.google.com/contact/2008#contact"/>
+//   <gd:name>
+//     <gd:givenName>New</gd:givenName>
+//     <gd:familyName>Name</gd:familyName>
+//     <gd:fullName>New Name</gd:fullName>
+//   </gd:name>
+//   <content type="text">Notes</content>
+//   <link rel="http://schemas.google.com/contacts/2008/rel#photo" type="image/*"
+//     href="https://www.google.com/m8/feeds/photos/media/viniciusferreirawk%40gmail.com/6976230c08eade30"/>
+//   <link rel="self" type="application/atom+xml"
+//     href="https://www.google.com/m8/feeds/contacts/viniciusferreirawk%40gmail.com/full/6976230c08eade30"/>
+//   <link rel="edit" type="application/atom+xml"
+//     href="https://www.google.com/m8/feeds/contacts/viniciusferreirawk%40gmail.com/full/6976230c08eade30"/>
+//   <gd:phoneNumber rel="http://schemas.google.com/g/2005#other"
+//     primary="true">456-123-2133</gd:phoneNumber>
+//   <gd:extendedProperty name="pet" value="hamster"/>
+//   <gContact:groupMembershipInfo deleted="false"
+//     href="http://www.google.com/m8/feeds/groupsviniciusferreirawk%40gmail.com/base/6"/>
+// </entry>
--- utils.php
+++ utils.php
@@ -7,16 +7,17 @@ $google_client_secret = 'pRItA1PTgdHcdO7On8pgWGTG';
 $google_redirect_uri = 'http://venizao.dlinkddns.com/google-api/retorno.php';
 
 #pegar dados do token no key-retorno.txt
-$auth_code 			 = '4/WgAKvyUNTUz9aoFGkA0NV5Tw565J2fOn4poWTdDmAvbdSnIGJL0t58vPprGpnu7pY0yXhQyDyPHHYwm_Hyh_ZZo';
+$auth_code 			 = '4/WgCQHOwQyGVd6qt87ztsBQslooKSiROoQvjOMOwPz3FOJDRqOTVf8zmoMZ0uopLwOsL1A1elZjRKLvPn6dZpEBU';
 
 #pegar access_code dentro de auth-retorno.txt
-$acess_code = 'ya29.GlsYBotpQxkxHoXWdkpa3fv37h04ODIuWiVRmci36UV-FwKoDMCJDBt-V7VULVF0sYVF5dchgUJTMy-fnCXTnRDj81JL6XzTv3CL0tYurfZb-rus49yUEYo9JPNO';
+$acess_code = 'ya29.GlsYBj-fEZvDpdkTsryia7Tu2TqcUYWTKwWTM8s_FkAzgmNeKCWkycJcykkTeUhbH01yPUStApZQQcA_Q8JIP-yK20-9bzTMx2OGksEjMqyvAnlpq8-BPxOs-jFO';
 
 
 #pegar id dentro de user-retorno.txt - http://www.google.com/m8/feeds/contacts/viniciusferreirawk%40gmail.com/base/391e6fcf09d3530b -> 391e6fcf09d3530b
-$idgmail 	 = '391e6fcf09d3530b';
+$idgmail 	 = '6976230c08eade30';
 $groupId 	 = '6';
-$userEmail 	 = 'viniciusferreirawk@gmail.com';
+#$userEmail 	 = 'viniciusferreirawk@gmail.com';
+$userEmail 	 = 'viniciusferreirawk%40gmail.com';
 
 #USAR PLAYGROUND PARA LIST O GRUPO My contacts e pegar o ID
 #http://www.google.com/m8/feeds/groups/viniciusferreirawk%40gmail.com/base/6
